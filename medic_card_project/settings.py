c47b3c94d07d0316c1246920e188de1ebb667a30
v.ivenin <iveninvala7@gmail.com>
2025-10-25 11:43:06 +0300

    Fix: fix png8

--- medic_card_project/settings.py
+++ medic_card_project/settings.py
@@ -127,9 +127,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"  # ← добавлено
 
 MEDIA_URL = "media/"
-# MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "media"
 # MEDIA_URL = '/media/'
-MEDIA_ROOT = '/srv/media'
+# MEDIA_ROOT = '/srv/media'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
