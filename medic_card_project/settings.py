7c4d57ec1014fd5bce549fefcb2e5f196d8ad8c6
v.ivenin <iveninvala7@gmail.com>
2025-11-04 11:35:28 +0300

    Fix: volume 3

--- medic_card_project/settings.py
+++ medic_card_project/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = "medic_card_project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db/db.sqlite3",
     }
 }
 
